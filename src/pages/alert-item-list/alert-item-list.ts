76c78d9641673793edda035393dc860b0fcc6092
Yongliang Zhan <yzhan94@gmail.com>
2016-12-12 16:03:36 +0100

    Ordena AlertItems por distancia

--- src/pages/alert-item-list/alert-item-list.ts
+++ src/pages/alert-item-list/alert-item-list.ts
@@ -6,6 +6,7 @@ import { NavController, LoadingController } from 'ionic-angular';
 
 import { ItemService } from '../../services/item.service';
 import { ImageService } from '../../services/image.service';
+import { LocationService, Location } from '../../services/location.service';
 
 import { Item } from '../../models/item';
 import { Image } from '../../models/image';
@@ -21,17 +22,20 @@ export class AlertItemListPage {
 
 	_itemsBackup: Item[];
 	items: Item[];
+	distances: Object;
 	covers: Object;
 
 	constructor(
-		public navCtrl: NavController,
-		public loadingCtrl: LoadingController,
-		public translate: TranslateService,
-		public itemService: ItemService,
-		public imageService: ImageService
+		private navCtrl: NavController,
+		private loadingCtrl: LoadingController,
+		private translate: TranslateService,
+		private itemService: ItemService,
+		private imageService: ImageService,
+		private locationService: LocationService
 	) {
 		this.items = [];
 		this.covers = {};
+		this.distances = {};
 		var loader = this.loadingCtrl.create();
 		loader.present();
 		Promise.all([
@@ -45,24 +49,42 @@ export class AlertItemListPage {
 	doRefresh(refresher?: any): void {
 		let itemCount = 0,
 				itemsLength = 0;
-		this.items = this.itemService.listAlertItems();
-		this._itemsBackup = this.itemService.listAlertItems();
-		itemsLength = this.items.length;
-		this.items.forEach((item) => {
-			let image: Observable<Image>;
-			if (item.images) {
-				let imageId = Object.keys(item.images)[0];
-				image = this.imageService.getObservableImage(imageId);
-			} else {
-				let img = new Image();
-				img.image = 'assets/img/no-image.png';
-				image = Observable.of(img);
-			}
-			this.covers[item.$key] = image;
-			itemCount++;
-			if (itemCount === itemsLength) {
-				if (refresher) refresher.complete();
-			}
+		this.getCurrentLocation().then((location: Location) => {
+			let R: number = 6371000; // Mean Earth radius in meters
+			let phiA: number = (location.latitude/180)*Math.PI;
+
+			this.items = this.itemService.listAlertItems();
+			this._itemsBackup = this.itemService.listAlertItems();
+
+			itemsLength = this.items.length;
+			this.items.forEach((item) => {
+				let image: Observable<Image>;
+
+				/* Calculate distance to current location */
+				let phiB: number = (item.location.latitude/180)*Math.PI;
+				let dphi: number = ((item.location.latitude - location.latitude)/180)*Math.PI;
+				let dlmb: number = ((item.location.longitude - location.longitude)/180)*Math.PI;
+				let a: number = Math.pow(Math.sin(dphi/2), 2) + Math.cos(phiA)*Math.cos(phiB)*Math.pow(Math.sin(dlmb/2), 2);
+				let d: number = R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+				this.distances[item.$key] = Math.round(d*100)/100;
+
+				if (item.images) {
+					let imageId = Object.keys(item.images)[0];
+					image = this.imageService.getObservableImage(imageId);
+				} else {
+					let img = new Image();
+					img.image = 'assets/img/no-image.png';
+					image = Observable.of(img);
+				}
+				this.covers[item.$key] = image;
+				itemCount++;
+				if (itemCount === itemsLength) {
+					if (refresher) refresher.complete();
+				}
+			});
+			this.items.sort((a, b) => {
+				return this.distances[a.$key] - this.distances[b.$key];
+			});
 		});
 	}
 
@@ -92,6 +114,15 @@ export class AlertItemListPage {
 		} else {
 			this.items = this._itemsBackup;
 		}
+	}
+
+	getCurrentLocation(): Promise<Location> {
+		return new Promise((resolve, reject) => {
+			let subscription = this.locationService.getCurrentLocation().subscribe((location) => {
+				subscription.unsubscribe();
+				resolve(location);
+			});
+		});
 	}
 
 }
